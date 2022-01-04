0000e539010927162b6a10e9fdf42980bd7fd347
Michael Dippery <michael@monkey-robot.com>
2022-01-03 23:03:22 -0800

    Alias ^f/^b to f/b
    These are Vim-isms that will be nice to have.

--- src/telescope/app.py
+++ src/telescope/app.py
@@ -9,8 +9,10 @@ class Telescope(App):
         await self.bind("q", "quit")
         # TODO: Figure out how to page with space
         await self.bind("f", "page_forward")
+        await self.bind(Keys.ControlF, "page_forward")
         await self.bind(Keys.PageDown, "page_forward")
         await self.bind("b", "page_back")
+        await self.bind(Keys.ControlB, "page_back")
         await self.bind(Keys.PageUp, "page_back")
         await self.bind("j", "select_next")
         await self.bind(Keys.Down, "select_next")
