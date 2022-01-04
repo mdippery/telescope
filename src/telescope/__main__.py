6c0dd142b0a0514761dd41c3b8349fd0dcbf9eb8
Michael Dippery <michael@monkey-robot.com>
2022-01-04 15:02:56 -0800

    Silence warnings about unclosed SSL sockets
    It's an issue with boto3/botocore and there's not much I can do about
    that (not that it really matters).

--- src/telescope/__main__.py
+++ src/telescope/__main__.py
@@ -1,4 +1,5 @@
 import os.path
+import warnings
 
 import boto3
 
@@ -6,6 +7,7 @@ from telescope.app import Telescope
 
 
 def main():
+    warnings.filterwarnings(action="ignore", message="unclosed", category=ResourceWarning)
     # TODO: Configure for non-Mac platforms
     log_path = os.path.join("~", "Library", "Logs", "aws-telescope.log")
     log_path = os.path.expanduser(log_path)
