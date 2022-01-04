58b69086fccbac46ed7a88ece2dc513bcf95044b
Michael Dippery <michael@monkey-robot.com>
2022-01-04 11:48:32 -0800

    Refactor pagination into base panel class
    This same logic will be used by the panel that shows bucket file lists
    as well.

--- src/telescope/gui.py
+++ src/telescope/gui.py
@@ -19,21 +19,13 @@ def StandardTable(*cols):
     return Table(*cols, box=None, expand=True, show_header=False)
 
 
-class BucketListPanel(Widget):
+class PagePanel(Widget):
     page = Reactive(1)
     selected_item = Reactive(0)
 
     def __init__(self, page):
         super().__init__()
         self.page = page
-        self._buckets = None
-
-    @property
-    def buckets(self):
-        if not self._buckets:
-            s3 = boto3.client("s3")
-            self._buckets = s3.list_buckets()["Buckets"]
-        return self._buckets
 
     @property
     def max_items_per_page(self):
@@ -42,12 +34,12 @@ class BucketListPanel(Widget):
     @property
     def first_item(self):
         if self.page == self.max_page:
-            return len(self.buckets) - self.max_items_per_page
+            return len(self.items) - self.max_items_per_page
         return self.max_items_per_page * (self.page - 1)
 
     @property
     def last_item(self):
-        return min(self.max_items_per_page * self.page, len(self.buckets)) - 1
+        return min(self.max_items_per_page * self.page, len(self.items)) - 1
 
     @property
     def min_page(self):
@@ -55,7 +47,7 @@ class BucketListPanel(Widget):
 
     @property
     def max_page(self):
-        return math.ceil(len(self.buckets) / self.max_items_per_page)
+        return math.ceil(len(self.items) / self.max_items_per_page)
 
     @property
     def min_item(self):
@@ -63,19 +55,7 @@ class BucketListPanel(Widget):
 
     @property
     def max_item(self):
-        return len(self.buckets) - 1
-
-    def render(self):
-        body = StandardTable("Name")
-
-        start, end = self.first_item, self.last_item + 1
-        for i, bucket in enumerate(self.buckets[start:end], start):
-            text = bucket["Name"]
-            if i == self.selected_item:
-                text = Text(text, style="reverse")
-            body.add_row(text)
-
-        return StandardPanel(body, title="Buckets")
+        return len(self.items) - 1
 
     def page_forward(self):
         if self.page + 1 <= self.max_page:
@@ -106,6 +86,33 @@ class BucketListPanel(Widget):
                 self.page_back()
         else:
             self.console.bell()
+
+
+class BucketListPanel(PagePanel):
+    def __init__(self, page):
+        super().__init__(page)
+        self._buckets = None
+
+    @property
+    def buckets(self):
+        if not self._buckets:
+            s3 = boto3.client("s3")
+            self._buckets = s3.list_buckets()["Buckets"]
+        return self._buckets
+
+    items = buckets
+
+    def render(self):
+        body = StandardTable("Name")
+
+        start, end = self.first_item, self.last_item + 1
+        for i, bucket in enumerate(self.buckets[start:end], start):
+            text = bucket["Name"]
+            if i == self.selected_item:
+                text = Text(text, style="reverse")
+            body.add_row(text)
+
+        return StandardPanel(body, title="Buckets")
 
 
 class HelpPanel(Widget):
