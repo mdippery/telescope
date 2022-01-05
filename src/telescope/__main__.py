cb77b2afe1f06a773558ba7ac9e45fc0469e549b
Michael Dippery <michael@monkey-robot.com>
2022-01-04 22:18:45 -0800

    Verify AWS credentials before starting TUI
    I have found it to be hard to catch the exception thrown by botocore in
    the context of Telescope.run(), so we'll verify the credentials before
    we start the TUI, so we can print a helpful error message and exit
    neatly.

--- src/telescope/__main__.py
+++ src/telescope/__main__.py
@@ -1,9 +1,12 @@
 import os
 import platform
+import sys
 import warnings
 from pathlib import Path
 
 import boto3
+import botocore.exceptions
+from rich import print
 
 from telescope.app import Telescope
 
@@ -19,8 +22,18 @@ def logdir():
         return Path.cwd()
 
 
+def verify_credentials():
+    sts = boto3.client("sts")
+    try:
+        _ = sts.get_caller_identity()
+    except botocore.exceptions.UnauthorizedSSOTokenError as exc:
+        print(str(exc), file=sys.stderr)
+        sys.exit(1)
+
+
 def main():
     warnings.filterwarnings(action="ignore", message="unclosed", category=ResourceWarning)
+    verify_credentials()
     log_path = logdir() / "aws-telescope.log"
     Telescope.run(title="Telescope", log=str(log_path))
 
